bb947c5132d134660e10abb99ad31ab1eaad7202
kingswwong <467271883@qq.com>
2019-07-15 23:16:20 +0800

    Change Other class member variable access rights to private

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -3,13 +3,25 @@ package org.katas.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    private String nm;
+    private String addr;
+    private List<LineItem> li;
 
     public Order(String nm, String addr, List<LineItem> li) {
         this.nm = nm;
         this.addr = addr;
+        this.li = li;
+    }
+
+    public void setNm(String nm) {
+        this.nm = nm;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public void setLi(List<LineItem> li) {
         this.li = li;
     }
 
